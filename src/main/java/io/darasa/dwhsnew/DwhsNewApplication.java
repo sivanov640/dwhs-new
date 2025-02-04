083f094135843cfe273eca812a050a52ccbe0dbc
isergei <sergei@darasa.io>
2025-02-04 10:37:27 +0400

    init

---
+++ src/main/java/io/darasa/dwhsnew/DwhsNewApplication.java
@@ -0,0 +1,13 @@
+package io.darasa.dwhsnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwhsNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DwhsNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/darasa/dwhsnew/DwhsNewApplicationTests.java
@@ -0,0 +1,13 @@
+package io.darasa.dwhsnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwhsNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
